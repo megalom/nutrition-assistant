814d08b3b36523b924047fafbdd82ab23b5c4912
megalom <megalom@list.ru>
2021-04-20 17:19:37 +0500

    add project structure and basic welcome files

---
+++ src/main/java/ru/megalom/nutritionassistant/config/MvcConfig.java
@@ -0,0 +1,15 @@
+package ru.megalom.nutritionassistant.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("welcome/welcome");
+        registry.addViewController("/").setViewName("welcome/welcome");
+        registry.addViewController("/login").setViewName("security/login");
+    }
+}
